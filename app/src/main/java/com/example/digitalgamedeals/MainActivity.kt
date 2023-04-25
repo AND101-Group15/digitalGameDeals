47340f3c56877f40c6d84279671bf5130f4cf2bb
samantha <samanthap8675@gmail.com>
2023-04-24 22:42:45 -0400

    search bar

--- app/src/main/java/com/example/digitalgamedeals/MainActivity.kt
+++ app/src/main/java/com/example/digitalgamedeals/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.digitalgamedeals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.digitalgamedeals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            print("hello")
+        }
     }
 }
