1df27f942c6ce41fda73cb15edaeef7221032f6a
yari-gut <ygutierrezyadi@gmail.com>
2023-04-27 19:54:46 -0400

    Changed layout of both activities. Created intent to change between activities. Brought game id to second activity

--- app/src/main/java/com/example/digitalgamedeals/GameAdapter.kt
+++ app/src/main/java/com/example/digitalgamedeals/GameAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.digitalgamedeals
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -10,24 +12,36 @@ import androidx.recyclerview.widget.RecyclerView
 class GameAdapter (private val gameTitleList: List<String>,
                    private val gamePriceList: List<String>,
                    private val gameIDList: List<String>): RecyclerView.Adapter<GameAdapter.ViewHolder>(){
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    class ViewHolder(view: View, listener: onItemClickListener) : RecyclerView.ViewHolder(view) {
+        val gameThumbnail: ImageButton
         val gameTitle: TextView
         val gamePrice: TextView
 
         init {
             // Find our RecyclerView item's ImageView for future use
-
+            gameThumbnail = view.findViewById(R.id.game_thumbnail)
             gameTitle = view.findViewById(R.id.game_title)
             gamePrice = view.findViewById(R.id.game_price)
+            itemView.setOnClickListener{
+                listener.onItemClick(adapterPosition)
+            }
         }
+    }
+
+    private lateinit var mListener : onItemClickListener
+    interface onItemClickListener {
+        fun onItemClick(position : Int)
+    }
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.game_item, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(view, mListener)
     }
 
     override fun getItemCount() = gameTitleList.size
