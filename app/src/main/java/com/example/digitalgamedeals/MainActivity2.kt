1fed3a0f47ff3d36de38ee8b2f4d50e8ba0ea7ec
samantha <samanthap8675@gmail.com>
2023-04-29 10:01:19 -0400

    sort button2

--- app/src/main/java/com/example/digitalgamedeals/MainActivity2.kt
+++ app/src/main/java/com/example/digitalgamedeals/MainActivity2.kt
@@ -1,12 +1,15 @@
 package com.example.digitalgamedeals
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -28,6 +31,8 @@ class MainActivity2 : AppCompatActivity() {
     private lateinit var gameRatingNumList: MutableList<String>
     private lateinit var gameRatingTextList: MutableList<String>
     private lateinit var rvDeals: RecyclerView
+    private lateinit var adapter: DealsAdapter
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,6 +62,16 @@ class MainActivity2 : AppCompatActivity() {
 
         if (search != null) {
             getDealInfo(search)
+        }
+
+
+        adapter = DealsAdapter(gameNormalPriceList, gameSalePriceList, gameStoreList, checkbox, gameThumbnailList, gameRatingNumList, gameRatingTextList)
+        val sort = findViewById<Button>(R.id.sort)
+        sort.setOnClickListener {
+            val search = searchTitle.text.toString()
+            getDealInfo(search)
+            gameSalePriceList.sortBy { it.toDouble() }
+            adapter.notifyDataSetChanged()
         }
 
 
